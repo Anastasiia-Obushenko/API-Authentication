12c3ca0b8f5aec98c90f52445af8121e78c15772
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-01-21 16:02:59 -0500

    user parser

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+//Import Routes
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 
 // Connect to DB
-
 mongoose.connect(
     process.env.DB_CONNECT,
     () => console.log("connected to db")
 );
 
-//Import Routes
-const authRoute = require('./routes/auth');
+// Middlewere
+app.use(express.json());
 
 //Route Middlewares
 app.use('/api/user', authRoute);
