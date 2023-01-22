901a652cf585f8a08b4b6ca5331dc356775205a6
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-01-22 18:53:03 -0500

    user validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const { registerValidation, loginValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
+    // Validate
+    const { error } = registerValidation(req.body);
+    if (error) { return res.status(400).send(error.details[0].message); }
+    // Check user in DB
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) { return res.status(400).send('Email already exist'); }
+    // Create new User
     const user = new User({
         name: req.body.name,
         email: req.body.email,
