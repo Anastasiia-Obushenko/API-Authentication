0d9c06c47928eae9080fefd69c2c94b53344580a
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-01-22 20:00:45 -0500

    password hash

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const { registerValidation, loginValidation } = require('../validation');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res) => {
     // Validate
@@ -9,11 +10,14 @@ router.post('/register', async (req, res) => {
     // Check user in DB
     const emailExist = await User.findOne({ email: req.body.email });
     if (emailExist) { return res.status(400).send('Email already exist'); }
+    // Hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
     // Create new User
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try {
